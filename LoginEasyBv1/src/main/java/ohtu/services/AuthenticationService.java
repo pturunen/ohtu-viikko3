0d88a61061a7c0477e1d67ff1b0465ff3d077ce7
pturunen <pturunen@ovi.com>
2014-03-29 15:15:21 +0200

    laskari3 tehtavat 3,4,5

---
+++ LoginEasyBv1/src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,64 @@
+package ohtu.services;
+
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+
+public class AuthenticationService {
+
+    private UserDao userDao;
+    private int minLengthForName = 3;
+    private int minLengthForWord = 8;
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        // validity check of username and password
+        boolean specialFound = false;
+        String aakkoset = "abcdefghijklmnopqrstuvxyz";
+        for(int i=0;i<username.length();i++){
+            if(!aakkoset.contains(Character.toString(username.charAt(i)))){
+                return true;
+            }
+        }
+        for(int j=0;j<password.length() && !specialFound;j++){
+            if (!Character.isAlphabetic(password.charAt(j)) ){
+                specialFound = true;
+            }
+        }
+        
+        if (username.length()< minLengthForName 
+                || password.length() < minLengthForWord 
+                || !specialFound){
+            return true;
+        }
+        return false;
+    }
+}
